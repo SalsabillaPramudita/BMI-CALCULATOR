347b833efb5b92535e8703036751dd65f555e691
SalsabillaPramudita <110925630+SalsabillaPramudita@users.noreply.github.com>
2022-10-08 16:06:08 +0700

    Add files via upload

---
+++ TPA002-BMI-CALCULATOR-SALSABILLA PRAMUDITA/script.js
@@ -0,0 +1,20 @@
+		function BMI() {
+			let h=document.getElementById('h').value;
+			let w=document.getElementById('w').value;
+			let bmi=w/(h/100*h/100);
+			let bmio=(bmi.toFixed(2).bold());
+			let status = "Which Means You are "
+			
+
+			document.getElementById("result").innerHTML="Your BMI is " + bmio;
+			if(bmio < 18.5){
+                
+					document.getElementById("hasil").innerHTML = status + "Underweight".bold();
+			}else if(bmio <= 24.9){
+				document.getElementById("hasil").innerHTML = status + "Normal".bold();
+			}else if(bmio <= 29.9){
+				document.getElementById("hasil").innerHTML = status + "Overweight".bold();
+			}else
+				document.getElementById("hasil").innerHTML = status + "Obesity".bold();
+			
+		}
